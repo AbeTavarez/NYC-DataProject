056b091f2796f54f8f5418ce907d9412215d29c4
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-05-26 23:56:01 -0400

    rendering some data

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-//* BUTTONS
+//* ===== BUTTONS
 const manBtn = document.getElementById('manhattan')
 const bxBtn = document.getElementById('bronx')
 const bkBtn = document.getElementById('brooklyn')
 const qnsBtn = document.getElementById('queens')
 const stIndBtn = document.getElementById('staten-island')
 
-//* input
+//* ===== INPUT
 const input = document.querySelector('input')
+//* Container div
+const container = document.getElementById('container')
 
-// fetch data from API
+//* ==========> fetch data from API
 const getData = (e) => {
     const numComplaints = input.value || 10
     const borough = e.target.id.toUpperCase()
     // fetch data
     fetch(`https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=${numComplaints}&agency=NYPD&borough=${borough}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        renderData(data)
+    })
     .catch(err => console.error(err))
 }
 
+//* DATA RENDER FUNCTION
+const renderData = (data) => {
+    data.map(item => {
+        const div = document.createElement('div')
+        const h4 = document.createElement('h4')
+        const p = document.createElement('p')
+        h4.textContent = item.descriptor
+        p.textContent = item.resolution_description
+        h4.appendChild(p)
+        div.appendChild(h4)
+        container.appendChild(div)
+    })
+}
+
+//* ==========> Listeners
 manBtn.addEventListener('click', getData)
 bxBtn.addEventListener('click', getData)
 bkBtn.addEventListener('click', getData)
